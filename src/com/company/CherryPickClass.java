e5092da52d287a2bedff4d86de19876a4f1e2c99
Koval Aleksandr <koval@hostco.ru>
2021-03-12 12:16:25 +0500

    Example cherry pick commit

---
+++ src/com/company/CherryPickClass.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * @author koval
+ */
+public class CherryPickClass {
+
+    private String message;
+
+    public CherryPickClass(){
+        message = "I'am example for cherry-pick";
+    }
+
+}
